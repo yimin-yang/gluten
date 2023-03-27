86239f8e92c7babc77377cfdc76b9901e7b782d7
yangyimin <yangyimin@meituan.com>
2023-03-27 15:12:35 +0800

    changed build script

--- dev/buildbundle-veloxbe.sh
+++ dev/buildbundle-veloxbe.sh
@@ -4,5 +4,5 @@ BASEDIR=$(dirname $0)
 source "$BASEDIR/builddeps-veloxbe.sh"
 
 cd $GLUTEN_DIR
-mvn clean package -Pbackends-velox -Pspark-3.2 -DskipTests
+mvn clean package -Pspark-3.2 -Pbackends-velox -DskipTests -Dspark32.version=3.2.0
 mvn clean package -Pbackends-velox -Pspark-3.3 -DskipTests
